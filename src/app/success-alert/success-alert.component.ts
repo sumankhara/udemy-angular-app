c74a58a08205a65f526f4245d9ede3aed4307ec6
Suman Khara <suman.khara@cerner.com>
2019-06-04 16:41:55 +0530

    first commit

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'success-alert',
+  template: '<h3>Success Alert</h3>',
+  styles: [`
+    h3 {
+      color: green;
+    }
+  `]
+})
+export class SuccessAlertComponent {
+
+}
